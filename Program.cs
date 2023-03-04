12d82ee10b16910a9fa31e4c63cac3a88d6fee70
Eduardo Oviedo-Blanco <eduardo.oviedo@gmail.com>
2023-03-03 21:33:17 -0600

    New program file content

--- Program.cs
+++ Program.cs
@@ -1,25 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
-int firstValue = 10; // Explicitly typed
-var secondValue = 5; // Implicitly typed
-secondValue = 6;
-firstValue = 11;
-
-Console.WriteLine(firstValue);  // 11
-
-int sum = firstValue + secondValue; 
-Console.WriteLine(sum); // 17
-
-int difference = firstValue - secondValue;
-Console.WriteLine(difference); // 5
-
-int product = firstValue * secondValue;
-Console.WriteLine(product); // 66
-
-int quotient = firstValue / secondValue;
-Console.WriteLine(quotient); // 1
-
-int remainder = firstValue % secondValue;
-Console.WriteLine(remainder); // 5
-
+class Program {
+    static void Main(string[] args) {
+        // code goes here! :)
+    }
+}
